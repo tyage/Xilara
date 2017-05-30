cf0bc65b9b769866e405fa3adc4946d11d964d99
tyage <namatyage@gmail.com>
2017-05-30 14:35:13 +0900

    mv package.json to top dir

--- test/webmin/index.js
+++ test/webmin/index.js
@@ -1,4 +1,5 @@
-import { roadrunnerFileToTemplate } from '../'
+import { roadrunnerFileToTemplate } from '../../roadrunner-to-template'
 
 const roadrunnerXMLFile = '../../vulnerable-apps/webmin/roadrunner/webmin00.xml'
 const template = roadrunnerFileToTemplate(roadrunnerXMLFile)
+console.log(template)
