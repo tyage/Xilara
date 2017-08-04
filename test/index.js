f8ad165df55ee942ff92c1e334fff36ee04ac4bb
tyage <namatyage@gmail.com>
2017-08-04 15:17:12 +0900

    fixed basic test to match with html preprocessing

--- test/index.js
+++ test/index.js
@@ -3,20 +3,31 @@ import assert from 'assert'
 import { isHTMLMatchWithTemplate } from '../template'
 import { Node, Tag, Optional, Loop, Ignore } from '../template/nodes'
 
+const createTag = (name, attrs, children) => {
+  const tag = new Tag(name, attrs || new Map())
+  if (children) {
+    tag.addChildren(children)
+  }
+  return tag
+}
+
 // div[id=main]
 //  |- ul[class=list,id=list]
 //     |- li
 //  |- p
 describe('HTML Tree', () => {
-  const ul = new Tag('ul', new Map([ ['id', 'list'], ['class', 'list'] ]))
-  ul.addChildren([ new Tag('li') ])
-  const div = new Tag('div', new Map([ ['id', 'main'] ]))
-  div.addChildren([
-    ul,
-    new Tag('p')
+  const template = createTag('html', null, [
+    createTag('head', null, []),
+    createTag('body', null, [
+      createTag('div', new Map([ ['id', 'main'] ]), [
+        createTag('ul', new Map([ ['id', 'list'], ['class', 'list'] ]), [
+          createTag('li')
+        ]),
+        createTag('p')
+      ])
+    ])
   ])
-  div.parent = new Node()
-  const template = div
+  template.parent = new Node()
 
   const matchedHTMLs = [
     '<div id="main"><ul id="list" class="list"><li>aaa</li></ul><p></p></div>',
@@ -25,9 +36,8 @@ describe('HTML Tree', () => {
   const notMatchedHTMLs = [
     '',
     '<div id="main"></div>',
-    // changing or removal of attr is ignored
-    //'<div><ul id="list" class="list"><li>aaa</li></ul><p></p></div>',
-    //'<div><ul id="nonlist" class="nonlist"><li>aaa</li></ul><p></p></div>',
+    '<div><ul id="list" class="list"><li>aaa</li></ul><p></p></div>',
+    '<div><ul id="nonlist" class="nonlist"><li>aaa</li></ul><p></p></div>',
     '<div id="main"><ul class="list" id="list"><li>aaa</li><p></p></ul></div>',
     '<div id="main"><ul class="list" id="list"><div>aaa</div></ul><p></p></div>'
   ]
