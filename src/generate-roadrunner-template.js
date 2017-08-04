283f885e9c7d0cd29a6269981b888142e4c6df67
tyage <namatyage@gmail.com>
2017-08-04 18:06:33 +0900

    execute roadrunner in submodule

--- src/generate-roadrunner-template.js
+++ src/generate-roadrunner-template.js
@@ -1,11 +1,14 @@
 import fs from 'fs'
-import { exec } from 'child_process'
+import { spawnSync } from 'child_process'
 import { formatHTMLByChrome } from './preprocessing'
 
 const buildDirBase = './build'
+const roadrunnerDir = './roadrunner'
 export const generateRoadRunnerTemplate = async (htmlFiles, preferenceFile) => {
   const buildDir = `${buildDirBase}/${(new Date()).getTime()}`
   fs.mkdirSync(buildDir)
+
+  // format html with chrome
   const formattedHTMLFiles = await Promise.all(htmlFiles.map(async (htmlFile, i) => {
     const html = fs.readFileSync(htmlFile).toString()
     const formattedHTML = await formatHTMLByChrome(html)
@@ -13,27 +16,16 @@ export const generateRoadRunnerTemplate = async (htmlFiles, preferenceFile) => {
     fs.writeFileSync(formattedHTMLFile, formattedHTML)
     return formattedHTMLFile
   }))
-  console.log(formattedHTMLFiles)
 
-  const result = exec(`
-cd ../roadrunner &&
-gradle run -Pargs='-Nwebmin -O${preferenceFile} ${formattedHTMLFiles.map(f => `../xss-auditor/${f}`).join(' ')}'
-`, (err, stdout, stderr) => {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (err !== null) {
-      console.log('exec error: ' + err);
-    }
+  // execute roadrunner
+  const result = spawnSync('./gradlew', [
+    `-Pargs=-Ntemplate -O../${preferenceFile} ${formattedHTMLFiles.map(f => `../${f}`).join(' ')}`,
+    'run'
+  ], {
+    cwd: roadrunnerDir
   })
-}
 
-/*
-generateRoadRunnerTemplate(
-  [
-    './vulnerable-apps/webmin/datasets/safe-1.html',
-    './vulnerable-apps/webmin/datasets/safe-2.html',
-    './vulnerable-apps/webmin/datasets/safe-3.html',
-  ],
-  './examples/webmin/webmin.xml'
-)
-*/
+  // move result to build dir
+  fs.renameSync(`${roadrunnerDir}/output/template/`, `${buildDir}/roadrunner`)
+  return `${buildDir}/roadrunner/template00.xml`
+}
