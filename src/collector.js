4953fb276c0e3a735d9dea5a2488ef0f83279def
tyage <namatyage@gmail.com>
2018-01-31 12:13:00 +0900

    set up proxy

--- src/collector.js
+++ src/collector.js
@@ -1,37 +1,37 @@
 import http from 'http';
+import zlib from 'zlib';
 import httpProxy from 'http-proxy';
 import { initDB, saveResponse } from './collector-db';
 
 const startServer = async () => {
-  const webServer = process.env.APP_URL;
-  const proxy = httpProxy.createServer();
-
   await initDB();
 
-  http.createServer((req, res) => {
+  const webServer = process.env.APP_URL;
+  const proxy = httpProxy.createServer({
+    target: webServer
+  });
+  proxy.listen(8000);
+
+  proxy.on('proxyRes', (proxyRes, req, res) => {
     console.log(`Receiving reverse proxy request for: ${req.url}`);
 
-    res.on('pipe', (proxyRes) => {
-      const contentType = proxyRes.headers['content-type'];
-      if (contentType === undefined || !(contentType.includes('text/html'))) {
-        return;
+    const contentType = proxyRes.headers['content-type'];
+    if (contentType === undefined || !(contentType.includes('text/html'))) {
+      return;
+    }
+
+    let data = Buffer.alloc(0);
+    proxyRes.on('data', (chunk) => {
+      data = Buffer.concat([data, chunk]);
+    });
+    proxyRes.on('end', async () => {
+      const isGzipped = /gzip/.test(proxyRes.headers['content-encoding']);
+      if (isGzipped) {
+        data = zlib.gunzipSync(data);
       }
 
-      let data = Buffer.alloc(0);
-      proxyRes.on('data', (chunk) => {
-        data = Buffer.concat([data, chunk]);
-      });
-      proxyRes.on('end', async () => {
-        const isGzipped = /gzip/.test(proxyRes.headers['content-encoding']);
-        if (isGzipped) {
-          data = zlib.gunzipSync(data);
-        }
-
-        await saveResponse(req.method, req.url, data);
-      });
+      await saveResponse(req.method, req.url, data);
     });
-
-    proxy.web(req, res, { target: webServer });
-  }).listen(8000);
+  });
 };
 startServer();
